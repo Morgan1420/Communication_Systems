6d1c3b8f6ee0a68fc4a79c715991e425621eb009
Morgan1420 <jan.moran.r@gmail.com>
2024-06-02 17:58:22 +0200

    plot arreglat

--- _generic_functions/plot_signal.m
+++ _generic_functions/plot_signal.m
@@ -35,21 +35,4 @@ function plot_signal(varargin) % Input args: signal, figure number, plot title,
         title(params.title_2);
     end
 
-
-    function params = parseOptionalParams(defaultParams, varargin)
-        % Create an input parser
-        p = inputParser;
-
-        % Add each field in the defaultParams struct as a parameter
-        paramNames = fieldnames(defaultParams);
-        for i = 1:length(paramNames)
-            addParameter(p, paramNames{i}, defaultParams.(paramNames{i}));
-        end
-
-        % Parse input arguments
-        parse(p, varargin{:});
-
-        % Return the parameters as a struct
-        params = p.Results;
-    end
 end
