776841caedcf038b4da8d902e8da0a47a7884870
Aleix Jorda <aleixjb121@gmail.com>
2024-06-03 13:02:30 +0200

    feat: Add demod_unipolar_rz and mod_unipolar_rz functions for demodulating and modulating unipolar RZ signals

---
+++ Digital/Demoduladors_bb/demod_unipolar_rz.m
@@ -0,0 +1,49 @@
+function signals = demod_unipolar_nrz(varargin)
+    % Parsegem els parèmtres per defecte
+    defaultParams = struct('r', [0 0], 'divisions_pols', 10, 'llista_valors', [2 0]);
+    params = parse_optional_params(defaultParams, varargin{:});
+
+    % Creem la estructura que retornarèm
+    signals = struct('h_r', [], 'y', [], 'Y', [], 'y_KT', [], 'Y_KT', [], 'a_KT', [], 'b_r', []);
+
+
+    % ----------------------------- Filtre receptor
+    signals.h_r = zeros(1, params.divisions_pols);
+    signals.h_r(1) = 1;
+
+    % ----------------------------- Senyal rebuda
+    signals.y = conv(params.r,signals.h_r);
+    signals.Y = transformada_fourier(signals.y);
+
+
+    % ----------------------------- Dades rebudes
+    signals.y_KT = [];
+    for i = (params.divisions_pols/2):params.divisions_pols+1:length(signals.y)
+        signals.y_KT = [signals.y_KT signals.y(i)];
+    end
+
+
+    % ----------------------------- Detector de nivell
+    signals.a_KT = [];
+    for i = 1:length(signals.y_KT)
+        signals.a_KT = [signals.a_KT find_closest_value(params.llista_valors, signals.y_KT(i))];
+    end
+
+    % ----------------------------- Decodificador de linea
+    signals.b_r = back_to_binary_unipolar(signals.a_KT);
+    signals.b_r = signals.b_r(1:2:end);
+
+    
+    % ----------------------------- Funcions extra
+    function missatge = back_to_binary_unipolar(a)
+        missatge = [];
+        for j = 1:length(a)
+            if a(j) > 0
+                missatge = [missatge 1];
+            else
+                missatge = [missatge 0];
+            end
+        end 
+        
+    end
+end
